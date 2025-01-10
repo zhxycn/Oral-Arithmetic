6d2d3cd85b11bd1edbfad746dec71ec69c852a1d
zhxy-CN <admin@owo.cab>
2025-01-11 00:31:03 +0800

    :sparkles: 用户信息页

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,13 +1,126 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import { getCookie } from '@/utils/cookie';
 
+const session = getCookie('session') || '';
+const uid = ref('');                                      // 用户 ID
+const email = ref('');                                    // 邮箱
+const nickname = ref<string | null>(null);                // 昵称
+const avatar = ref('');                                   // 头像
+const total = ref(0);                                     // 总场次
+const competition_total = ref(0);                         // PK 总场次
+const competition_win = ref(0);                           // PK 胜场次
+const qid = ref<number[]>([]);                            // 测试 ID
+const mistake = ref<[]>([]);                              // 错题
+
+const fetchUserData = async () => {
+  if (!process.env.API_URL) {
+    console.error('API_URL is not defined');
+    alert('API_URL environment variable not found');
+    return;
+  }
+
+  try {
+    const response = await axios.get(`${process.env.API_URL}/user?type=get`, {
+      withCredentials: true
+    });
+    const userdata = response.data;
+    uid.value = userdata["uid"];
+    email.value = userdata["email"];
+    nickname.value = userdata["nickname"];
+    avatar.value = userdata["avatar"];
+    total.value = userdata["total"];
+    competition_total.value = userdata["competition_total"];
+    competition_win.value = userdata["competition_win"];
+    qid.value = userdata["qid"];
+    mistake.value = userdata["mistake"];
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(() => {
+  if (session) {
+    fetchUserData();
+  }
+});
 </script>
 
 <template>
-  <div>
-
+  <div class="profile-container">
+    <img :src="`${avatar}`" class="profile-avatar" alt=""/>
+    <div class="profile-info">
+      <h2>欢迎，{{ nickname }}</h2>
+      <table class="profile-table">
+        <tr>
+          <td>UID:</td>
+          <td>{{ uid }}</td>
+        </tr>
+        <tr>
+          <td>邮箱:</td>
+          <td>{{ email }}</td>
+        </tr>
+        <tr>
+          <td>总场次:</td>
+          <td>{{ total }}</td>
+        </tr>
+        <tr>
+          <td>PK 总场次:</td>
+          <td>{{ competition_total }}</td>
+        </tr>
+        <tr>
+          <td>PK 胜场次:</td>
+          <td>{{ competition_win }}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.profile-container {
+  display: flex;
+  align-items: center;
+  padding: 2rem;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(160, 160, 160, 0.1);
+  max-width: 800px;
+  margin: 2rem auto;
+}
 
+.profile-avatar {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  margin: 0 4rem 0 1rem;
+  border: 2px solid #ccc;
+}
+
+.profile-info {
+  display: flex;
+  flex-direction: column;
+}
+
+.profile-info h2 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+  color: #333;
+}
+
+.profile-table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.profile-table td {
+  padding: 0.5rem;
+  font-size: 1.2rem;
+  color: #555;
+}
+
+.profile-info strong {
+  color: #333;
+}
 </style>
